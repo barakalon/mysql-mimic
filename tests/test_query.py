a116b5893320b35c9a3b7f49f73c36eb351b674f
cecily_carver <cecily.carver@airbnb.com>
2025-01-07 14:12:06 -0800

    Adding link to PR with INSTR() fix.

--- tests/test_query.py
+++ tests/test_query.py
@@ -779,6 +779,7 @@ async def test_describe_select(
             ],
         ),
         # Re-enable this test after sqlglot releases fix.
+        # PR with fix: https://github.com/tobymao/sqlglot/pull/4574
         # (
         #     "select database(), schema(), left(user(),instr(concat(user(),'@'), '@')-1)",
         #     [{"DATABASE()": None, "SCHEMA()": None, "_col_2": "levon_helm"}],
